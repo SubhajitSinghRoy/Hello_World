954a9c4d9d7765dbe263a462000f8ba88b1dbf1c
SSR <subhojits74@gmail.com>
2021-06-27 21:02:08 +0530

    First commit

---
+++ src/com/First/FirstBuild.java
@@ -0,0 +1,11 @@
+package com.First;
+
+public class FirstBuild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Yeah!!! My first Build is successful");
+	}
+
+}
